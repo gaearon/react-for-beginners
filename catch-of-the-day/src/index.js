4d7ed0be118f6a4b59b22dc4807052e2683b3d97
interglobalmedia <interglobalmedia@gmail.com>
2017-02-22 12:55:55 -0500

    Add new changes.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-/* we are only importing the named render() method because we do not need the entire ReactDOM package. */
 import { render } from 'react-dom';
 import './css/style.css';
 import App from './components/App';
 
 import StorePicker from './components/StorePicker';
 
-/* render out component. then what DOM element should it render out to?  */
 render(<App/>, document.querySelector('#main'));
