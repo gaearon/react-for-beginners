935c1bcebe860f30786536b67822dd83ce208cc5
interglobalmedia <interglobalmedia@gmail.com>
2017-02-22 13:24:57 -0500

    Moved .gitignore into catch-of-the-day.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,7 +6,7 @@ import Inventory from './Inventory';
 class App extends React.Component {
 	render() {
 		return (
-            <div className=".catch-of-the-day">
+            <div className="catch-of-the-day">
                 <div className="menu">
                     <Header />
                 </div>
