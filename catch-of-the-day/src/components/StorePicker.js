33eeadef0a5b0f31e6339daa85197f69eca0ea68
interglobalmedia <interglobalmedia@gmail.com>
2017-02-22 19:07:25 -0500

    Completed fist 10 videos. Into 4:06 minutes of 11. Adding notes.txt to repo and new component called NotFound.js.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
     // every component you build needs at least one method and that's the render method
+	goToStore() {
+		console.log('You changed the URL');
+		// first grab text from box
+		// second transition from / to /store/storeId
+	}
 	render() {
 		return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.gotToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store →</button>
             </form>
 		)
