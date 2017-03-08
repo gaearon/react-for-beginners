4d4934b545f1d77636722d2b84d9eae9d33e1736
interglobalmedia <interglobalmedia@gmail.com>
2017-03-08 06:34:19 -0500

    Up to video 12.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,16 +3,20 @@ import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
     // every component you build needs at least one method and that's the render method
-	goToStore() {
+	goToStore(event) {
+		// prevents page refresh
+		event.preventDefault();
 		console.log('You changed the URL');
 		// first grab text from box
+		console.log(this.storeInput.value);
 		// second transition from / to /store/storeId
 	}
 	render() {
+		// Anywhere else
 		return (
-            <form className="store-selector" onSubmit={this.gotToStore}>
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
                 <button type="submit">Visit Store →</button>
             </form>
 		)
